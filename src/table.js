a0bce5fc3670062448d5ef5d04508cadd358067c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-03-04 11:59:05 -0600

    save initial table to file

--- src/table.js
+++ src/table.js
@@ -1,3 +1,50 @@
+import {saveTable} from './utilities/file';
+
+const dbNames = new WeakMap();
+const tableData = new WeakMap();
+const tableNames = new WeakMap();
+
 export class Table {
+
+  get dbName() {
+
+    return dbNames.get(this);
+
+  }
+
+  get tableData() {
+
+    return tableData.get(this);
+
+  }
+
+  get tableName() {
+
+    return tableNames.get(this);
+
+  }
+
+  constructor(dbName, tableName) {
+
+    dbNames.set(this, dbName);
+    tableNames.set(this, tableName);
+    tableData.set(this, []);
+
+  }
+
+  save() {
+
+    return new Promise((resolve, reject) => {
+
+      saveTable(
+        this.dbName,
+        this.tableName,
+        this.tableData,
+        err => (err ? reject : resolve)()
+      );
+
+    });
+
+  }
 
 }
--- test/table.js
+++ test/table.js
@@ -1,19 +1,45 @@
 import {Table} from '../src/table';
 import {expect} from 'code';
+import sinon from 'sinon';
+import * as fileService from '../src/utilities/file';
 
 describe('Given Table', () => {
 
-  let table;
+  const dbName = 'dbName';
+  const tableName = 'tableName';
+  let sandbox,
+    table;
 
   beforeEach(() => {
 
-    table = new Table();
+    sandbox = sinon.sandbox.create();
+
+    sandbox.stub(fileService, 'saveTable');
+
+    table = new Table(dbName, tableName);
 
   });
+
+  afterEach(() => sandbox.restore());
 
   it('should be constructed and used as an object', () => {
 
     expect(table).object();
+
+  });
+
+  it('should create the table with empty array on save', () => {
+
+    table.save();
+
+    sinon.assert.calledOnce(fileService.saveTable);
+    sinon.assert.calledWithExactly(fileService.saveTable, dbName, tableName, [], sinon.match.func);
+
+  });
+
+  it('should return a promise on save', () => {
+
+    expect(table.save()).instanceOf(Promise);
 
   });
 
